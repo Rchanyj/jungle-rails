1b36c4f84bd8c5ba0cc2ad7e5c814aed30047665
Raionos <rchan1992@gmail.com>
2017-11-11 06:24:57 +0000

    bug fixes; restricted review submits to logged in users

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,40 @@
 class ReviewsController < ApplicationController
+  before_action :require_login
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.create(review_params)
+
+    @review.user_id = current_user.id
+
+    if @review.save
+      redirect_to :controller=>'products', :action=>"show", :id=>@product.id
+    else
+      redirect_to :controller=>'products', :action=>"show", :id=>@product.id
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(
+      :user_id,
+      :product_id,
+      :description,
+      :rating
+    )
+  end
+
+  def destroy
+    @review =
+    @review.destroy
+    redirect_to :controller=>'products', :action=>"show", :id=>@product.id
+  end
+
+  private
+
+  def require_login
+    unless current_user
+      flash[:error] = "You must be logged in to comment!"
+    end
+  end
+
 end
