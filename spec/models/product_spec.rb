ef265c804656d1c83ec73468acc41d6108205453
Raionos <rchan1992@gmail.com>
2017-11-13 21:04:41 +0000

    refined tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -12,28 +12,28 @@ RSpec.describe Product, type: :model do
       @category = Category.new
       @product = Product.new(name: nil, price: 40.00, quantity: 2, category: @category)
       @product.save
-      expect(@product.errors[:name].size).to eq(1)
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'does not save without a price' do
       @category = Category.new
       @product = Product.new(name: "Bobby", price: nil, quantity: 2, category: @category)
       @product.save
-      expect(@product.errors[:price].size).to eq(2)
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end
 
     it 'does not save without a quantity' do
       @category = Category.new
       @product = Product.new(name: "Bobby", price: 50.00, quantity: nil, category: @category)
       @product.save
-      expect(@product.errors[:quantity].size).to eq(1)
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it 'does not save without a category' do
       @category = Category.new
       @product = Product.new(name: "Bobby", price: 50.00, quantity: 2, category: nil)
       @product.save
-      expect(@product.errors[:category].size).to eq(1)
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
   end
 end
